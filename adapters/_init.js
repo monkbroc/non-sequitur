b6c5ef1900a4f256b80dc708c41775558ce329cd
Julien Vanier <jvanier@gmail.com>
2016-12-24 12:12:00 -0500

    Update DB env variable

--- adapters/_init.js
+++ adapters/_init.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-var url = process.env.MONGODB_URL || 'mongodb://localhost/non-sequitur';
+var url = process.env.MONGO_URL || 'mongodb://localhost/non-sequitur';
 mongoose.connect(url);
 
